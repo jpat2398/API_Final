a4c4fbd68ab382744ae99c488c5cb01d030f9af4
jeron.patterson <patterson.jss98@gmail.com>
2023-08-23 10:24:40 +0100

    create repo

---
+++ src/test/java/Main.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class Main {
+
+    @Test
+    public void getProducts() {
+        
+    }
+}
