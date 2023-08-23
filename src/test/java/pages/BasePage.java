06abbf3da9701675984972f90ff58ba4af12a54d
Ezekiel Kokumo <ezekiel.kokumo@centre4.local>
2023-08-23 11:41:19 +0100

    Invalid URL/endpoint results in 404

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import java.util.Random;
+
+public class BasePage {
+
+    public static final String PATH = "/";
+
+    public String generatePath() {
+        Random rnd = new Random();
+        String randomString = generateRandomString(4, rnd);
+        String path = PATH + randomString;
+        return path;
+
+    }
+    private String generateRandomString(int length, Random rnd) {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = rnd.nextInt(characters.length());
+            sb.append(characters.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
