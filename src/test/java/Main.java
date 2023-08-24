0576c008743cf1f0235b6b3751ec7fafd85b770a
Ezekiel Kokumo <ezekiel.kokumo@centre4.local>
2023-08-24 11:03:24 +0100

    changess

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -31,7 +31,7 @@ public class Main {
     @Test
     public void checkEndpoint(){
         Response response = RestAssured.get(basePage.generatePath());
-g        response.then().assertThat().statusCode(404);
+       response.then().assertThat().statusCode(404);
     }
 
     @Test
