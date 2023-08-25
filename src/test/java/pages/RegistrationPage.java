da4f84282726c0a800971f8456e093611de4b22b
James Forcer <james.forcer@centre4.local>
2023-08-25 09:56:14 +0100

    commiting src file

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,50 +1,94 @@
 package pages;
 
+import com.sun.tools.javac.Main;
 import io.restassured.RestAssured;
+import io.restassured.filter.cookie.CookieFilter;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Random;
 
 public class RegistrationPage {
 
-    public String emailRandomiser(){
+
+    private BasePage basePage = new BasePage();
+    @BeforeClass
+    public static void basePageSetup (){
+        RestAssured.baseURI = "http://3.11.77.136/";
+
+    }
+
+    public String emailRandomiser() {
         Random randomGenerator = new Random();
         int randominteger = randomGenerator.nextInt(328517329);
         return "Tester" + randominteger + "@test.com";
+
     }
 
-    public String firstNameRandomiser(){
-        Random random = new Random();
-        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder firstname = new StringBuilder();
-        for (int i = 0; i < firstname.length(); i++) {
-            int randomIndex = random.nextInt(characters.length());
-            firstname.append(characters.charAt(randomIndex));
-    }
-
-    public String surnameRandomiser(){
-        Random surnamerandomiser = new Random();
-        String lettersAndNumbers = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder surname = new StringBuilder();
-        for (int i = 0; i < surname.length(); i++) {
-            int randomIndex = surnamerandomiser.nextInt(lettersAndNumbers.length());
-            surname.append(lettersAndNumbers.charAt(randomIndex));
-    }
-
-    public String passwordGenerator(){
-        Random passwordrandomiser = new Random();
-        String figures = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder password = new StringBuilder();
-        for (int i = 0; i < password.length(); i++) {
-            int randomIndex = passwordrandomiser.nextInt(figures.length());
-            password.append(figures.charAt(randomIndex));
-    }
     @Test
-    public void registerAsAUser() {
-        RestAssured.baseURI = "http://3.11.77.136/";
-        RestAssured.given()
+    public void registerANewUser() {
+        CookieFilter cookieFilter = new CookieFilter();
+        HashMap<String, String> newUserCreds = new HashMap<String, String>();
+        newUserCreds.put("id_customer", "");
+        newUserCreds.put("id_gender", "1");
+        newUserCreds.put("firstname", "Tester");
+        newUserCreds.put("lastname", "McTestFace");
+        newUserCreds.put("email", emailRandomiser());
+        newUserCreds.put("password", "Password1");
+        newUserCreds.put("birthday", "05/24/2000");
+        newUserCreds.put("submitCreate", "1");
+        Response response = RestAssured
+                .given().filter(cookieFilter)
+                .queryParam("controller", "authentication")
+                .queryParam("create_account", "1")
+                .formParams(newUserCreds)
                 .when()
-                .post(emailRandomiser();
+                .post("?controller=authentication&create_account=1");
+        System.out.println(response.statusCode());
+        System.out.println(cookieFilter.getCookieStore());
+        Response getNewAccount = RestAssured
+                .given().filter(cookieFilter)
+                 .queryParam("controller", "authentication")
+                 .queryParam("create_account", "1")
+                .get("?controller=authentication&create_account=1");
+        System.out.println(getNewAccount.getBody().prettyPrint());
+        System.out.println(cookieFilter.getCookieStore());
+        Assert.assertEquals("Tester McTestFace", getNewAccount.body().htmlPath().getString("**.findAll { it.@class== 'user-info' }.a.span"));
+    }
 
+    @Test
+    public void alreadyRegisteredUser(){
+
+        CookieFilter cookieFilter = new CookieFilter();
+        HashMap<String, String> newUserCreds = new HashMap<String, String>();
+        newUserCreds.put("id_customer", "");
+        newUserCreds.put("id_gender", "1");
+        newUserCreds.put("firstname", "Tester");
+        newUserCreds.put("lastname", "McTestFace");
+        newUserCreds.put("email", "tester@test.com");
+        newUserCreds.put("password", "Password1");
+        newUserCreds.put("birthday", "05/24/2000");
+        newUserCreds.put("submitCreate", "1");
+        Response response = RestAssured
+                .given().filter(cookieFilter)
+                .queryParam("controller", "authentication")
+                .queryParam("create_account", "1")
+                .formParams(newUserCreds)
+                .when()
+                .post("?controller=authentication&create_account=1");
+        System.out.println(response.statusCode());
+        System.out.println(cookieFilter.getCookieStore());
+        Response getNewAccount = RestAssured
+                .given().filter(cookieFilter)
+                .queryParam("controller", "authentication")
+                .queryParam("create_account", "1")
+                .get("?controller=authentication&create_account=1");
+        System.out.println(getNewAccount.getBody().prettyPrint());
+        System.out.println(cookieFilter.getCookieStore());
+        Assert.assertEquals("The email tester@test.com is already used, please choose another one or sign in", getNewAccount.body().htmlPath().getString("**.findAll { @class== 'alert alert-danger' }.span"));
     }
 }
